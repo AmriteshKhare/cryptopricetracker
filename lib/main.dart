d59ccfe04cc87e52ccd6ce01781188567d52bc04
Amritesh Khare <54805586+AmriteshKhare@users.noreply.github.com>
2023-05-27 12:42:14 +0530

    optimisation

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'cubit/cubit_service.dart';
 
 void main() {
   runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
       //Bloc Provider to maintain the Cubit state throughout the app
       home: BlocProvider<CryptoCubit>(
           create: (context) => CryptoCubit()..getCrypto(), child: HomePage())));
@@ -52,6 +53,7 @@ class _HomePageState extends State<HomePage> {
                   Text(
                     "Prices",
                     style: TextStyle(fontSize: 23, fontWeight: FontWeight.bold),
+                    textAlign: TextAlign.center,
                   ),
                   TextButton.icon(
                     onPressed: () {
@@ -126,155 +128,35 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.white,
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.black.withOpacity(.60),
-        selectedFontSize: 14,
-        unselectedFontSize: 14,
-        onTap: (value) {
-          // Respond to item press.
-        },
-        items: [
-          BottomNavigationBarItem(
-            label: "",
-            icon: Icon(Icons.bar_chart),
-          ),
-          BottomNavigationBarItem(
-            label: "",
-            icon: Icon(Icons.blur_circular_rounded),
-          ),
-          BottomNavigationBarItem(
-            label: "",
-            icon: Icon(Icons.notifications_none),
-          ),
-          BottomNavigationBarItem(
-            label: "",
-            icon: Icon(Icons.person_outline),
-          ),
-        ],
-      ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   type: BottomNavigationBarType.fixed,
+      //   backgroundColor: Colors.white,
+      //   selectedItemColor: Colors.black,
+      //   unselectedItemColor: Colors.black.withOpacity(.60),
+      //   selectedFontSize: 14,
+      //   unselectedFontSize: 14,
+      //   onTap: (value) {
+      //     // Respond to item press.
+      //   },
+      //   items: [
+      //     BottomNavigationBarItem(
+      //       label: "",
+      //       icon: Icon(Icons.bar_chart),
+      //     ),
+      //     BottomNavigationBarItem(
+      //       label: "",
+      //       icon: Icon(Icons.blur_circular_rounded),
+      //     ),
+      //     BottomNavigationBarItem(
+      //       label: "",
+      //       icon: Icon(Icons.notifications_none),
+      //     ),
+      //     BottomNavigationBarItem(
+      //       label: "",
+      //       icon: Icon(Icons.person_outline),
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
-
-
-
-
-
-// import 'package:flutter/material.dart';
-
-// void main() {
-//   runApp(const MyApp());
-// }
-
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // This is the theme of your application.
-//         //
-//         // Try running your application with "flutter run". You'll see the
-//         // application has a blue toolbar. Then, without quitting the app, try
-//         // changing the primarySwatch below to Colors.green and then invoke
-//         // "hot reload" (press "r" in the console where you ran "flutter run",
-//         // or simply save your changes to "hot reload" in a Flutter IDE).
-//         // Notice that the counter didn't reset back to zero; the application
-//         // is not restarted.
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: const MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   const MyHomePage({super.key, required this.title});
-
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-
-//   final String title;
-
-//   @override
-//   State<MyHomePage> createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
-//     return Scaffold(
-//       appBar: AppBar(
-//         // Here we take the value from the MyHomePage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
-//         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Invoke "debug painting" (press "p" in the console, choose the
-//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//           // to see the wireframe for each widget.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
